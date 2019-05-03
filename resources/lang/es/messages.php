d0e837167356cabb34bb2c7b693a02eded975a5f
Nelson Alejandro Saz <nelsonalejandrosaz@gmai.com>
2019-05-03 12:48:29 -0600

    Iniciando la base
    Agregando todo lo necesario para iniciar el proyecto

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mensajes generales del sistema
+    |--------------------------------------------------------------------------
+    |
+    | Mensajes de la aplicación
+    |
+    */
+
+    'empresa'               => 'Empresa S.A. de C.V',
+    'empresaAbreviado'      => 'EMP',
+    'desarrolladoPor'       => 'Desarrollador y mantenido por:',
+    'desarrollador'         => 'Sistemas & Sistemas S.A. de C.V.'
+
+];
---
+++ resources/views/vendor/adminlte/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <div class="pull-right">
+        {{trans('messages.desarrolladoPor')}} <a href="http://sistemasysistemasv.com">{{ trans('messages.desarrollador') }}</a>
+    </div>
+    <strong>Copyright &copy;<script>document.write(new Date().getFullYear());</script> <a href="http://acacha.org">{{trans('messages.empresa')}}</a>.</strong><br>
+</footer>
